c556c41a5d7630168b72069cb283fbe2ec7737eb
Matti Schneider <matti.schneider@data.gouv.fr>
2015-03-16 15:54:51 +0100

    Retrocompatibility for Mongo connection definition

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var possibleValues = config.possibleValues;
 delete config.possibleValues;	// avoid sending unused data to UI
 
 
-process.env.MONGODB_URL = require('url').format({
+process.env.MONGODB_URL = process.env.MONGODB_URL || require('url').format({
 	protocol	: 'mongodb',
 	slashes		: true,
 	host		: process.env.npm_package_config_mongoHost
